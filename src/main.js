a697fd3e9b85a934eddc56b746bf52d82a465336
penggang <918800642@qq.com>
2018-07-09 15:14:01 +0800

    添加axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 import $ from 'jquery'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,6 +13,24 @@ import headers from '@/components/public/header'
 console.log($)
 Vue.config.productionTip = false
 Vue.component('headers', headers)
+Vue.prototype.$http = axios
+// 添加一个请求拦截器
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+console.log(axios)
+// 添加一个响应拦截器
+axios.interceptors.response.use(function (response) {
+  // Do something with response data
+  return response
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error)
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
